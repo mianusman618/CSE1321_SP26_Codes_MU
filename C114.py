0b16752b7cbe812b07ed5e62aaf9b349519e33b0
muhammadusman <usman_fsd618@yahoo.com>
2026-04-01 14:16:54 -0400

    Commit 04/01/2026 (2)

---
+++ C114.py
@@ -0,0 +1,20 @@
+class Student:
+    name=""
+    id=""
+    courses=[]
+    def func1(self,P2,P3=100):
+        pass
+    def func2(self,P2,P3,P4):
+        pass
+
+
+s1=Student()
+s2=Student()
+
+s1.name="Alice"
+s2.name="Bob"
+
+print(s1.name)
+print(s2.name)
+
+print(s1)
---
+++ C115.py
@@ -0,0 +1,4 @@
+user_input=input("Enter some input ")
+print(user_input)
+user_input_list=user_input.split(" ")
+print(user_input_list)
